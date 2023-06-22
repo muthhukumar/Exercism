ef8f87bab5af08024a3e142da3878b9f0d3b0e84
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-06-22 08:10:41 +0530

    refactor(luhn): minor refactoring of luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -2,32 +2,26 @@
 
 pub fn is_valid(code: &str) -> bool {
     let not_space = |c: &char| b' ' != *c as u8;
-    if code.chars().any(|c| !(b' ' == c as u8) && !c.is_digit(10)) {
+    let not_space_or_digit = |c: char| !(b' ' == c as u8) && !c.is_digit(10);
+
+    if code.chars().any(not_space_or_digit) {
         return false;
     };
-    let code = {
-        let code: Vec<i32> = code
-            .chars()
-            .filter(not_space)
-            .filter(|c| c.is_numeric())
-            .map(|c| c.to_digit(10).unwrap_or(0) as i32)
-            .collect();
-        if code.len() <= 1 {
-            return false;
-        };
-        code
-    };
-    let result: Vec<i32> = code
-        .iter()
+
+    let code: Vec<i32> = code
+        .chars()
+        .filter(not_space)
+        .map(|c| c.to_digit(10).unwrap_or(0) as i32)
         .rev()
         .enumerate()
         .map(|(idx, v)| match idx % 2 {
-            0 => *v,
-            _ => match *v * 2 {
+            0 => v,
+            _ => match v * 2 {
                 d if d > 9 => d - 9,
                 d => d,
             },
         })
         .collect();
-    result.iter().sum::<i32>() % 10 == 0
+
+    code.len() > 1 && code.iter().sum::<i32>() % 10 == 0
 }
