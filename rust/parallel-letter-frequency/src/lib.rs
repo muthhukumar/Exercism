25a2a31cc012a18afbbc8c81def1dcb955596746
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-06-24 08:41:24 +0530

    improved perf

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
 
 // TODO - use a separate thread for sending message. This thread should convert the input like
@@ -17,60 +17,73 @@ fn str_char_count(str: &str, count: &mut HashMap<char, usize>) {
     }
 }
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+pub fn frequency_without_worker(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
     let mut char_count: HashMap<char, usize> = HashMap::new();
 
     for str in input.iter() {
         str_char_count(str, &mut char_count);
-        // for c in str
-        //     .chars()
-        //     .map(|c| c.to_ascii_lowercase())
-        //     .filter(|c| c.is_alphanumeric() && !c.is_digit(10))
-        // {
-        //     let count = char_count.entry(c).or_insert(0);
-        //
-        //     *count += 1;
-        // }
     }
 
     char_count
-    //
-    // let mut handles = vec![];
-    //
-    // let idx = Arc::new(Mutex::new(0));
-    // let input = Arc::new(&input);
-    // let char_count: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
-    //
-    // for _ in 1..worker_count + 1 {
-    //     let idx = Arc::clone(&idx);
-    //     let char_count = Arc::clone(&char_count);
-    //     let input = Arc::clone(&input);
-    //
-    //     let handle = thread::spawn(move || {
-    //         // let result = idx.lock().unwrap();
-    //         // if let MutexGuard(idx) = idx.lock().unwrap() {
-    //         if let Some(input) = input.get(0) {
-    //             for c in input
-    //                 .chars()
-    //                 .map(|c| c.to_ascii_lowercase())
-    //                 .filter(|c| c.is_alphanumeric() && !c.is_digit(10))
-    //             {
-    //                 char_count.lock().unwrap().entry(c).or_insert(0);
-    //
-    //                 // *count += 1;
-    //             }
-    //         };
-    //     });
-    //
-    //     handles.push(handle);
-    // }
-    //
-    // for handle in handles {
-    //     handle.join().unwrap();
-    // }
-    //
-    // match char_count.lock() {
-    //     Ok(char_count) => char_count.clone(),
-    //     Err(_) => HashMap::new() as HashMap<char, usize>,
-    // }
+}
+
+pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    let mut works = vec![];
+
+    let mut start = 0;
+    if input.len() >= worker_count {
+        for i in 1..worker_count + 1 {
+            let work_count = {
+                if input.len() % worker_count == 0 {
+                    input.len() / worker_count
+                } else {
+                    if i == worker_count {
+                        (input.len() / worker_count) + (input.len() % worker_count)
+                    } else {
+                        input.len() / worker_count
+                    }
+                }
+            };
+
+            works.push([start, start + work_count]);
+
+            start = start + work_count;
+        }
+    } else {
+        works.push([0, input.len()]);
+    }
+
+    let input = input.clone().to_owned();
+
+    let tf_input: Arc<RwLock<Vec<String>>> =
+        Arc::new(RwLock::new(input.iter().map(|s| s.to_string()).collect()));
+
+    let char_count: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    let mut handles = vec![];
+
+    for [start, end] in works {
+        let cloned_tf_input = Arc::clone(&tf_input);
+        let char_count = Arc::clone(&char_count);
+
+        let handle = thread::spawn(move || {
+            let input = cloned_tf_input.read().unwrap();
+            if let Some(val) = input.get(start..end) {
+                for str in val {
+                    let mut char_count = char_count.lock().unwrap();
+                    str_char_count(str, &mut char_count);
+                }
+            };
+        });
+
+        handles.push(handle)
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let x = char_count.lock().unwrap().to_owned();
+
+    x
 }
