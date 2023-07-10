e3a06bd9955e8bf2d79a96c06f130c459c3d861a
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-10 21:59:40 +0530

    feat(diff-of-sqr): used fold instead of formula

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,17 @@
-pub fn square_of_sum(n: u32) -> u32 {
-    let sum_of_n = (n * (n + 1)) / 2;
+pub fn square_of_sum_formula(n: u32) -> u32 {
+    ((n * (n + 1)) / 2).pow(2)
+}
 
-    sum_of_n * sum_of_n
+pub fn sum_of_squares_formula(n: u32) -> u32 {
+    (n * (n + 1) * (2 * n + 1)) / 6
+}
+
+pub fn square_of_sum(n: u32) -> u32 {
+    (1..=n).into_iter().fold(0, |acc, x| acc + x).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (n * (n + 1) * (2 * n + 1)) / 6
+    (1..=n).into_iter().fold(0, |acc, x| acc + (x * x))
 }
 
 pub fn difference(n: u32) -> u32 {
