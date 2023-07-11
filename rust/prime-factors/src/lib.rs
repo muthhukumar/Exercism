0fcafca51ea0a3433dd3aeb3692915250a74b935
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-11 19:53:55 +0530

    refactor(prime-factors): removed prime number stuff

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,19 +1,3 @@
-fn is_prime(n: u64) -> bool {
-    if n <= 2 {
-        return true;
-    }
-
-    let end = (n as f64).sqrt() as u64 + 1;
-
-    for i in 2..=end {
-        if n % i == 0 {
-            return false;
-        }
-    }
-
-    true
-}
-
 pub fn factors(n: u64) -> Vec<u64> {
     let end = (n as f64).sqrt() as u64 + 1;
 
@@ -24,11 +8,9 @@ pub fn factors(n: u64) -> Vec<u64> {
     let range = (2..=end).into_iter();
 
     range.fold(&mut result, |acc, i| {
-        if is_prime(i) {
-            while num % i == 0 {
-                acc.push(i);
-                num = num / i;
-            }
+        while num % i == 0 {
+            acc.push(i);
+            num = num / i;
         }
 
         acc
