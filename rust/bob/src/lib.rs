5a686bf972f678fdf8572cd552d3729ddf3cc9b4
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-19 21:23:39 +0530

    maybe tomorrow im bored

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,6 +1,25 @@
 pub fn reply(message: &str) -> &str {
-    match message {
-        x if x.contains('?') => "Sure.",
-        _ => "Whatever",
+    let message: Vec<char> = message.chars().collect();
+
+    println!("Input: {:?}", message);
+
+    if message.iter().all(|c| c.is_whitespace()) {
+        return "Fine. Be that way!";
+    } else if message
+        .iter()
+        .all(|c| c.is_numeric() || c.is_uppercase() || !c.is_alphanumeric() && *c != '?')
+    {
+        return "Whoa, chill out!";
+    } else if message.iter().all(|c| c.is_uppercase() || *c == '?') {
+        return "Calm down, I know what I'm doing!";
     }
+
+    if let Some(last) = message.iter().filter(|c| c.is_whitespace()).last() {
+        if *last == '?' {
+            println!("list: {}", *last);
+            return "Sure.";
+        }
+    }
+
+    return "Whatever.";
 }
