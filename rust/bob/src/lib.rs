7fe23409e71a177659f98237517106bc0b47bda4
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-21 20:47:18 +0530

    refactor(bob): improved

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,33 +1,26 @@
 pub fn reply(message: &str) -> &str {
-    let message: Vec<char> = message.chars().filter(|c| !c.is_whitespace()).collect();
+    let message_char: Vec<char> = message.chars().filter(|c| !c.is_whitespace()).collect();
 
-    println!("{:?}", message);
-
-    if message.len() == 0 {
+    if message_char.len() == 0 {
         return "Fine. Be that way!";
     }
 
-    if message
-        .iter()
-        .filter(|c| c.is_alphabetic())
-        .all(|c| c.is_uppercase())
-        && !message.iter().any(|c| *c == '?')
-        && message.iter().filter(|c| c.is_alphabetic()).count() > 0
-    {
-        return "Whoa, chill out!";
-    }
+    let mes = message_char.iter().filter(|c| c.is_alphabetic());
 
-    if message
-        .iter()
-        .filter(|c| c.is_alphabetic())
-        .all(|c| c.is_uppercase())
-        && *message.last().unwrap() == '?'
-        && message.iter().filter(|c| c.is_alphabetic()).count() > 0
+    if mes.clone().all(|c| c.is_uppercase())
+        && *message_char.last().unwrap() == '?'
+        && mes.count() > 0
     {
         return "Calm down, I know what I'm doing!";
     }
 
-    if *message.last().unwrap() == '?' {
+    let mes = message_char.iter().filter(|c| c.is_alphabetic());
+
+    if mes.clone().all(|c| c.is_uppercase()) && mes.count() > 0 {
+        return "Whoa, chill out!";
+    }
+
+    if *message_char.last().unwrap() == '?' {
         return "Sure.";
     }
 
